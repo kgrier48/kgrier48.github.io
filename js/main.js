bcb9406270ad8ab359fb473b40c7818b1afebbd9
kgrier48 <grierkag@gmail.com>
2018-12-15 22:39:24 -0500

    General Styling

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$( document ).ready(function() {
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 200) {
+        $(".navbar").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".navbar").removeClass("active");
+    }
+});
+
+});
